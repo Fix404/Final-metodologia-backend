74f195a53b9de67f785062ebb8fdad266e7fe430
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2025-04-20 20:07:29 -0300

    primer commit del backend. persistence.xml

---
+++ src/main/java/org/example/final_metodologia_backend/FinalMetodologiaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.final_metodologia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalMetodologiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalMetodologiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/final_metodologia_backend/FinalMetodologiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.final_metodologia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalMetodologiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
